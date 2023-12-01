96cb68b443d87401f0fd1add4cd200eb0b5c8cbe
XYL0neer <abeilharz9@gmail.com>
2023-12-01 16:06:23 +0100

    solve day01 #1

--- 01/main.go
+++ 01/main.go
@@ -3,11 +3,35 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
-	fmt.Println(input)
+	var lines = strings.Split(input, "\n")
+
+	var numbers = []int{}
+
+	for _, line := range lines {
+		var first, last = -1, -1
+		for i, v := range line {
+			if v >= '0' && v <= '9' {
+				if first < 0 {
+					first = i
+				}
+				last = i
+			}
+		}
+		var n, _ = strconv.Atoi(fmt.Sprintf("%c%c", line[first], line[last]))
+		numbers = append(numbers, n)
+	}
+
+	var sum = 0
+	for _, n := range numbers {
+		sum += n
+	}
+	fmt.Println(sum)
 }
